0b3636d78bac0b5fe19cf24ed0db3a442681fd59
parawana <parawana@iu.edu>
2025-11-06 01:16:08 -0500

    added replayability

--- Projects/21 Black Jack Game/Game Files V1.py
+++ Projects/21 Black Jack Game/Game Files V1.py
@@ -35,89 +35,95 @@ def calculate_hand_value(hand):
         print('Ace is now a 1!')
     return total
 
-### Shuffle the deck
-random.shuffle(deck_of_cards)
 
-### Card dealing
-Player = []
-Dealer = []
+while True:
+    ### Shuffle the deck
+    random.shuffle(deck_of_cards)
 
-for i in deck_of_cards:
-    if len(Player) <= 1:
-        card = deck_of_cards.pop(0)
-        Player.append(card)
-        print(f"Dealt {card} to Player")
-    
-    if len(Dealer) == 0:
-        card = deck_of_cards.pop(0)
-        Dealer.append(card)
-        print(f"Dealt {card} to Dealer")
-    elif len(Dealer) == 1:
-        card = deck_of_cards.pop(0)
-        Dealer.append(card)
-        print("Dealt ?? to Dealer")
+    ### Card dealing
+    Player = []
+    Dealer = []
 
-### player and dealer total should go here as this is when the player and dealer variables come in
-player_total = calculate_hand_value(Player)
-dealer_total = calculate_hand_value(Dealer)
-### Added the hidden card feature for the dealer
-dealer_display = [Dealer[0], '??']
+    for i in deck_of_cards:
+        if len(Player) <= 1:
+            card = deck_of_cards.pop(0)
+            Player.append(card)
+            print(f"Dealt {card} to Player")
+        
+        if len(Dealer) == 0:
+            card = deck_of_cards.pop(0)
+            Dealer.append(card)
+            print(f"Dealt {card} to Dealer")
+        elif len(Dealer) == 1:
+            card = deck_of_cards.pop(0)
+            Dealer.append(card)
+            print("Dealt ?? to Dealer")
 
-print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
-print('Dealer = ' + str(dealer_display) + ', ' + 'Total = ??')
+    ### player and dealer total should go here as this is when the player and dealer variables come in
+    player_total = calculate_hand_value(Player)
+    dealer_total = calculate_hand_value(Dealer)
+    ### Added the hidden card feature for the dealer
+    dealer_display = [Dealer[0], '??']
 
-# This is just a check to make sure that it works correctly which it does
-# print(len(deck_of_cards))
+    print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
+    print('Dealer = ' + str(dealer_display) + ', ' + 'Total = ??')
 
-### Autowin off of draw condition
-if player_total == 21:
-   win_counter += 1
-   print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
-if dealer_total == 21:
-   loss_counter += 1
-   print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+    # This is just a check to make sure that it works correctly which it does
+    # print(len(deck_of_cards))
 
-### Player Hit, Stand Choices
-while player_total < 21:
-    player_decision = input("Would you like to Hit or Stand? ")
-    
-### hit vs stand cont
-    if player_decision.lower() == 'hit':
-        card = deck_of_cards.pop(0)
-        Player.append(card)
-        print(f"Dealt {card} to Player")
-        player_total = calculate_hand_value(Player)
-        print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
-    elif player_decision.lower() == 'stand':
-        break
-
-print('Dealer = ' + str(dealer_display) + ', ' + 'Total = ??')
-
-if player_total > 21:
-   loss_counter += 1
-   print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
-
-### Player side done now onto dealers turn
-if player_total <= 21:
-    print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
-
-    while dealer_total < 17:
-        card = deck_of_cards.pop(0)
-        Dealer.append(card)
-        print(f"Dealt {card} to Dealer")
-        dealer_total = calculate_hand_value(Dealer)
-        print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
-    
-    if dealer_total > 21:
+    ### Autowin off of draw condition
+    if player_total == 21:
         win_counter += 1
         print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
-        ### if dealer stops and player doesn't bust
-    else:
-        if player_total > dealer_total:
+    if dealer_total == 21:
+        loss_counter += 1
+        print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+
+    ### Player Hit, Stand Choices
+    while player_total < 21:
+        player_decision = input("Would you like to Hit or Stand? ")
+        
+    ### hit vs stand cont
+        if player_decision.lower() == 'hit':
+            card = deck_of_cards.pop(0)
+            Player.append(card)
+            print(f"Dealt {card} to Player")
+            player_total = calculate_hand_value(Player)
+            print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
+        elif player_decision.lower() == 'stand':
+            break
+
+    print('Dealer = ' + str(dealer_display) + ', ' + 'Total = ??')
+
+    if player_total > 21:
+        loss_counter += 1
+        print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+
+    ### Player side done now onto dealers turn
+    if player_total <= 21:
+        print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
+
+        while dealer_total < 17:
+            card = deck_of_cards.pop(0)
+            Dealer.append(card)
+            print(f"Dealt {card} to Dealer")
+            dealer_total = calculate_hand_value(Dealer)
+            print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
+        
+        if dealer_total > 21:
             win_counter += 1
             print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
-        elif player_total < dealer_total:
-            loss_counter += 1
-            print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+            ### if dealer stops and player doesn't bust
         else:
-            print('It is a push')
+            if player_total > dealer_total:
+                win_counter += 1
+                print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
+            elif player_total < dealer_total:
+                loss_counter += 1
+                print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+            else:
+                print('It is a push')
+
+    play_again = input('Do you want to play another hand? ')
+    if play_again.lower() != 'yes':
+        break
