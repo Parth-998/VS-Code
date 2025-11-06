99e9062006859d6f05597930c311ebd2cae585ef
parawana <parawana@iu.edu>
2025-11-06 01:11:00 -0500

    21 BlackJack Basic V1 Done.

--- Projects/21 Black Jack Game/Game Files V1.py
+++ Projects/21 Black Jack Game/Game Files V1.py
@@ -78,22 +78,46 @@ if dealer_total == 21:
    print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
 
 ### Player Hit, Stand Choices
-if player_total <= 21:
+while player_total < 21:
     player_decision = input("Would you like to Hit or Stand? ")
     
-### If for Hit
-    if player_decision == 'Hit' or 'hit':
+### hit vs stand cont
+    if player_decision.lower() == 'hit':
         card = deck_of_cards.pop(0)
         Player.append(card)
         print(f"Dealt {card} to Player")
         player_total = calculate_hand_value(Player)
+        print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
+    elif player_decision.lower() == 'stand':
+        break
 
-if player_total >= 21:
-   loss_counter += 1
-   print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
-if dealer_total == 21:
-   loss_counter += 1
-   print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
-
-print('Player = ' + str(Player) + ', ' + 'Total = ' + str(player_total))
 print('Dealer = ' + str(dealer_display) + ', ' + 'Total = ??')
+
+if player_total > 21:
+   loss_counter += 1
+   print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+
+### Player side done now onto dealers turn
+if player_total <= 21:
+    print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
+
+    while dealer_total < 17:
+        card = deck_of_cards.pop(0)
+        Dealer.append(card)
+        print(f"Dealt {card} to Dealer")
+        dealer_total = calculate_hand_value(Dealer)
+        print('Dealer = ' + str(Dealer) + ', ' + 'Total = ' + str(dealer_total))
+    
+    if dealer_total > 21:
+        win_counter += 1
+        print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
+        ### if dealer stops and player doesn't bust
+    else:
+        if player_total > dealer_total:
+            win_counter += 1
+            print('You Win!' + ', ' + 'Wins: ' + str(win_counter))
+        elif player_total < dealer_total:
+            loss_counter += 1
+            print('You Lose :(' + ', ' + 'Losses: ' + str(loss_counter))
+        else:
+            print('It is a push')
